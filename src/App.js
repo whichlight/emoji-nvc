2f3b1d17dfca1c1ce8af76a65fd527f4b940db56
whichlight <whichlight@gmail.com>
2022-08-02 16:33:09 -0400

    moving things into a loop

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Picker from "emoji-picker-react";
 //external before internal libs
 // "the further the way from your project, the higher it is. I like it." ~noman
 
+//todo
+//turn this into a loop to clean things up a bit
+
 import logo from "./logo.svg";
 import "./App.css";
 
@@ -28,7 +31,7 @@ const Prompt = (props) => {
         <input
           className="input-style"
           value={props.input}
-          onChange={props.setInput}
+          onChange={() => props.setInput}
           onClick={() => props.setPicker((val) => !val)}
         />
         {props.showPicker && <Picker onEmojiClick={onEmojiClick} />}
@@ -38,63 +41,69 @@ const Prompt = (props) => {
 };
 
 const App = () => {
+  let showPickers = Array(4).fill(null);
+  let setShowPickers = Array(4).fill(null);
+  let prompts = Array(4).fill(null);
+  let setPrompts = Array(4).fill(null);
+  const labels = ["when u ", "i feel ", "bc i need ", "would u pls "];
+
   const [showPicker1, setShowPicker1] = useState(false);
   const [prompt1, setPrompt1] = useState("");
+
+  showPickers[0] = showPicker1;
+  setShowPickers[0] = setShowPicker1;
+  prompts[0] = prompt1;
+  setPrompts[0] = setPrompt1;
 
   const [showPicker2, setShowPicker2] = useState(false);
   const [prompt2, setPrompt2] = useState("");
 
+  showPickers[1] = showPicker2;
+  setShowPickers[1] = setShowPicker2;
+  prompts[1] = prompt2;
+  setPrompts[1] = setPrompt2;
+
   const [showPicker3, setShowPicker3] = useState(false);
   const [prompt3, setPrompt3] = useState("");
 
+  showPickers[2] = showPicker3;
+  setShowPickers[2] = setShowPicker3;
+  prompts[2] = prompt3;
+  setPrompts[2] = setPrompt3;
+
   const [showPicker4, setShowPicker4] = useState(false);
   const [prompt4, setPrompt4] = useState("");
+
+  showPickers[3] = showPicker4;
+  setShowPickers[3] = setShowPicker4;
+  prompts[3] = prompt4;
+  setPrompts[3] = setPrompt4;
+
+  const promptsFill = showPickers.map((e, i) => {
+    return (
+      <Prompt
+        key={labels[i]}
+        label={labels[i]}
+        input={prompts[i]}
+        setInput={setPrompts[i]}
+        showPicker={showPickers[i]}
+        setPicker={setShowPickers[i]}
+      />
+    );
+  });
+
+  const displayResponses = prompts.map((e, i) => {
+    return <span key={i}>{e}</span>;
+  });
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="form-wrapper">
           <h2>emoji nvc</h2>
-
-          <Prompt
-            label="when u"
-            input={prompt1}
-            setInput={setPrompt1}
-            showPicker={showPicker1}
-            setPicker={setShowPicker1}
-          />
-
-          <Prompt
-            label="i feel "
-            input={prompt2}
-            setInput={setPrompt2}
-            showPicker={showPicker2}
-            setPicker={setShowPicker2}
-          />
-
-          <Prompt
-            label="bc i need "
-            input={prompt3}
-            setInput={setPrompt3}
-            showPicker={showPicker3}
-            setPicker={setShowPicker3}
-          />
-
-          <Prompt
-            label="would u pls "
-            input={prompt4}
-            setInput={setPrompt4}
-            showPicker={showPicker4}
-            setPicker={setShowPicker4}
-          />
-
+          {promptsFill}
           <button className="save-button">save</button>
-          <div>
-            <span>{prompt1}</span>
-            <span>{prompt2}</span>
-            <span>{prompt3}</span>
-            <span>{prompt4}</span>
-          </div>
+          <div>{displayResponses}</div>
         </div>
       </header>
     </div>
